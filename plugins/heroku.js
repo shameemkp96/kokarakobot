90b76725fc600f3fe48ca8da8467ff0c96a5d138
Farhan_Dqz <kaztrobabalover@gmail.com>
2021-12-21 21:45:36 -0800

    fix

--- plugins/heroku.js
+++ plugins/heroku.js
@@ -8,7 +8,6 @@ WhatsJulie - Yusuf Usta
 const Julie = require('../events');
 const Config = require('../config');
 const Heroku = require('heroku-client');
-const {secondsToHms} = require('./afk');
 const got = require('got');
 const {MessageType} = require('@adiwajshing/baileys');
 const sql = require('./sql/greetings');
@@ -183,73 +182,6 @@ Julie.addCommand({pattern: 'shutdown$', fromMe: true, dontAddCommandList: true, 
 }));
 
 
-if (Config.WORKTYPE == 'private') {
-
-    Julie.addCommand({pattern: 'dyno$', fromMe: true, dontAddCommandList: true, desc: Lang.DYNO_DESC}, (async (message, match) => {
-
-        heroku.get('/account').then(async (account) => {
-            // have encountered some issues while calling this API via heroku-client
-            // so let's do it manually
-            url = "https://api.heroku.com/accounts/" + account.id + "/actions/get-quota"
-            headers = {
-                "User-Agent": "Chrome/80.0.3987.149 Mobile Safari/537.36",
-                "Authorization": "Bearer " + Config.HEROKU.API_KEY,
-                "Accept": "application/vnd.heroku+json; version=3.account-quotas",
-            }
-            await got(url, {headers: headers}).then(async (res) => {
-               const resp = JSON.parse(res.body);
-               total_quota = Math.floor(resp.account_quota);
-               quota_used = Math.floor(resp.quota_used);         
-               percentage = Math.round((quota_used / total_quota) * 100);
-               remaining = total_quota - quota_used;
-               await message.client.sendMessage(
-                    message.jid,
-                    Lang.DYNO_TOTAL + ": ```{}```\n\n".format(secondsToHms(total_quota))  + 
-                    Lang.DYNO_USED + ": ```{}```\n".format(secondsToHms(quota_used)) +  
-                    Lang.PERCENTAGE + ": ```{}```\n\n".format(percentage) +
-                    Lang.DYNO_LEFT + ": ```{}```\n".format(secondsToHms(remaining)),
-                    MessageType.text
-               );
-            }).catch(async (err) => {
-                await message.client.sendMessage(message.jid,err.message, MessageType.text);     
-            });        
-        });
-    }));
-}
-else if (Config.WORKTYPE == 'public') {
-
-    Julie.addCommand({pattern: 'dyno$', fromMe: false, dontAddCommandList: true, desc: Lang.DYNO_DESC}, (async (message, match) => {
-
-        heroku.get('/account').then(async (account) => {
-            // have encountered some issues while calling this API via heroku-client
-            // so let's do it manually
-            url = "https://api.heroku.com/accounts/" + account.id + "/actions/get-quota"
-            headers = {
-                "User-Agent": "Chrome/80.0.3987.149 Mobile Safari/537.36",
-                "Authorization": "Bearer " + Config.HEROKU.API_KEY,
-                "Accept": "application/vnd.heroku+json; version=3.account-quotas",
-            }
-            await got(url, {headers: headers}).then(async (res) => {
-               const resp = JSON.parse(res.body);
-               total_quota = Math.floor(resp.account_quota);
-               quota_used = Math.floor(resp.quota_used);         
-               percentage = Math.round((quota_used / total_quota) * 100);
-               remaining = total_quota - quota_used;
-               await message.client.sendMessage(
-                    message.jid,
-                    Lang.DYNO_TOTAL + ": ```{}```\n\n".format(secondsToHms(total_quota))  + 
-                    Lang.DYNO_USED + ": ```{}```\n".format(secondsToHms(quota_used)) +  
-                    Lang.PERCENTAGE + ": ```{}```\n\n".format(percentage) +
-                    Lang.DYNO_LEFT + ": ```{}```\n".format(secondsToHms(remaining)),
-                    MessageType.text
-               );
-            }).catch(async (err) => {
-                await message.client.sendMessage(message.jid,err.message, MessageType.text);     
-            });        
-        });
-    }));
-}
-
 Julie.addCommand({pattern: 'setvar ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.SETVAR_DESC}, (async(message, match) => {
 
     if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.KEY_VAL_MISSING, MessageType.text);
@@ -351,33 +283,6 @@ Julie.addCommand({pattern: 'setvar ?(.*)', fromMe: true, dontAddCommandList: tru
                 'I Can\'t Add Owner to Block Chat',
                 MessageType.text
             );
-        }
-    }
-    if (match[1] == 'BLOCK_CHAT: false' || match[1] == 'BLOCK_CHAT: False' || match[1] == 'BLOCK_CHAT: FALSE' || match[1] == 'BLOCK_CHAT:False' || match[1] == 'BLOCK_CHAT:FALSE' || match[1] == 'BLOCK_CHAT:fakse' || match[1] == 'BLOCK_CHAT: fakse' || match[1] == 'BLOCK_CHAT:falde' || match[1] == 'BLOCK_CHAT: falde' || match[1] == 'BLOCK_CHAT:flase' || match[1] == 'BLOCK_CHAT:Flase' || match[1] == 'BLOCK_CHAT: flase') {
-
-        if (Config.LANG == 'TR' || Config.LANG == 'AZ') {
-            await message.client.sendMessage(
-                message.jid,
-                '_Görünüşe göre_ *BLOCK_CHAT* _anahtarını_ *false* _yapmaya çalışıyorsun._\n_Merak etme, senin için doğrusunu ayarlayabilirim._',
-                MessageType.text
-            );
-            return await heroku.patch(baseURI + '/config-vars', {
-                body: {
-                    ['BLOCK_CHAT']: 'false'
-                }
-            });
-        }
-        else {
-            await message.client.sendMessage(
-                message.jid,
-                '_It looks like you are trying to make the_ *BLOCK_CHAT* _var switch_ *false.*\n_Dont worry, I will set it for you._',
-                MessageType.text
-            );
-            return await heroku.patch(baseURI + '/config-vars', {
-                body: {
-                    ['BLOCK_CHAT']: 'false'
-                }
-            });
         }
     }
     if (match[1] == 'DEBUG: true' || match[1] == 'DEBUG: True' || match[1] == 'DEBUG: TRUE' || match[1] == 'DEBUG:True' || match[1] == 'DEBUG:TRUE' || match[1] == 'DEBUG:ture' || match[1] == 'DEBUG: ture' || match[1] == 'DEBUG:ttue' || match[1] == 'DEBUG:trie' || match[1] == 'DEBUG: trie' || match[1] == 'DEBUG:Trie' || match[1] == 'DEBUG: Trie') {
